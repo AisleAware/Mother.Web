eb5aa712c0d4d7bdf2bf050a5338811d78773090
Clay Smith <clay@intellens.com>
2020-06-05 23:44:17 -0500

    Added user claims selection

---
+++ Models/ClaimsStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Mother.Web.Models
+{
+    public static class ClaimsStore
+    {
+        public static List<Claim> AllClaims = new List<Claim>()
+        {
+            // Define the type of available claims
+            new Claim("Create Role", "Create Role"),
+            new Claim("Edit Role", "Edit Role"),
+            new Claim("Delete Role", "Delete Role")
+        };
+    }
+}
